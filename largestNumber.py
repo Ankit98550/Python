b5df12d62bb4729c53877bd261f8a1a27d7dbe2e
Ankit98550 <146458365+Ankit98550@users.noreply.github.com>
2026-08-08 00:14:47 +0530

    Check largest number from three number

---
+++ largestNumber.py
@@ -0,0 +1,19 @@
+# Find the largest number from three numbers
+def LargestNumber():
+    try:
+        a=int(input("Enter First Number : "))
+        b=int(input("Enter Second Number : "))
+        c=int(input("Enter Third Number : "))
+
+    except ValueError:
+        print("Please Enter a Integer Value")
+    if a==b==c: 
+        print("All values are Equal")
+    elif a>=b and a>=c:
+        print(f"{a} is greater than {b} and {c}")
+    elif b>=a and b>=c:
+        print(f"{b} is greater than {a} and {c}")
+    else:
+        print(f"{c} is greater than {a} and {b}")
+
+LargestNumber()
